01fc5da1f4e3527143fa75bd59ff436704767e4d
Spider1109 <ankittewary5012@gmail.com>
2024-03-04 23:52:27 +0530

    important files uploaded

---
+++ websiteclone1/script.js
@@ -0,0 +1,45 @@
+// typing animation
+var typed= new Typed(".typing",{
+    strings: ["Web Designer","Web Development","Graphic Designer","Youtuber"],
+    typespeed: 100,
+    Backspeed: 60,
+    loop: true
+})
+
+// Aside
+const nav = document.querySelector(".nav"),
+    navlist= nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+    for(let i=0; i<totalNavList; i++)
+    {
+        const a= navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+            for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.remove("back-section");
+            }
+            for(let j=0; j<totalNavList; j++)
+            {
+                if(navList[j].querySelector("a").classList.contains("active"))
+                {
+                    // console.log("back-sesction" + navList[j].querySelector("a"))
+                    allSection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+            showSection(this);
+        })
+    }
+    function showSection(element)
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#", target).classList.add("active")
+    }

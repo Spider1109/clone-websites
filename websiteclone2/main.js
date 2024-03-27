bf5a00ec85b9b266865bf7e5da70349fbc3d2317
Spider1109 <ankittewary5012@gmail.com>
2024-03-27 18:24:08 +0530

    portfolio popup completed

--- websiteclone2/main.js
+++ websiteclone2/main.js
@@ -18,3 +18,29 @@ tabs.forEach(tab =>
             target.classList.add('skills_active')
         })
     })
+
+// link acitve work
+const linkwork=document.querySelectorAll('.work_items')
+function activeWork(){
+    linkwork.forEach(i=> l.classlist.remove('active-work'))
+    this.classList.add('active-work')
+}
+linkwork.forEach(i=>l.addEventListener("click",activeWork))
+
+// workpopup
+document.addEventListener("click",(e)=>{
+    if(e.target.classlist.contains("work_button")){
+        toggleProtfolioPopup();
+        portfolioItemDetails(e.target.parentElement)
+    }
+})
+function toggleProtfolioPopup(){
+    document.querySelector(".portfolio_popup").classlist.toggle("open")
+}
+document.querySelector("portfolio_popup-close").addEventListener("click",togglePortfolioPopup)
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp_thumnbnail img").src=portfolioItem.querySelector(".work_img").src;
+    document.querySelector(".portfolio_popup-subtitle span"). innerHTML = portfolioItem.querySelector("work_title").innerHTML;
+    document.querySelector(".portfolio_popup-body"). innerHTML = portfolioItem.querySelector("portfolio_item-details").innerHTML;
+}
